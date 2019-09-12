5317087416652749cabe126b5e5992af69161308
matheus mahnke <matheusmahnke@gmail.com>
2019-09-11 22:43:38 -0300

    commit inicial

---
+++ PeerOne/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PeerOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        // GET api/default
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "teste", "endpoint1" };
+        }
+    }
+}
